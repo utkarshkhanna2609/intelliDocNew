1f3f54830dd5a99fef59fe15f5a6801e7bc32f16
utkarshkhanna2609 <utkarshkhannaaisn@gmail.com>
2023-08-18 16:29:29 +0530

    Firebase solution

--- server/firebase-admin.js
+++ server/firebase-admin.js
@@ -1,9 +1,28 @@
 
 
-import admin from 'firebase-admin';
-import serviceAccount from '../firebaseServiceAccount.json'; // Path to your service account key JSON file
-
+import admin from 'firebase-admin'; // Path to your service account key JSON file
+import dotenv from 'dotenv';
 let firebaseApp;
+
+
+
+
+dotenv.config();
+const serviceAccount = {
+  type: process.env.GC_TYPE,
+  project_id: process.env.GC_PROJECT_ID,
+  private_key_id: process.env.GC_PRIVATE_KEY_ID,
+  private_key: process.env.GC_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  client_email: process.env.GC_CLIENT_EMAIL,
+  client_id: process.env.GC_CLIENT_ID,
+  auth_uri: process.env.GC_AUTH_URI,
+  token_uri: process.env.GC_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.GC_AUTH_PROVIDER_CERT_URL,
+  client_x509_cert_url: process.env.GC_CLIENT_CERT_URL,
+};
+
+
+
 
 try {
   console.log("firebase-manager admin get app");
